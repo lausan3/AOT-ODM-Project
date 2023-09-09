e5786f08773ceb8dfa21b0efa6959945566934cd
lausan3 <lauanthony4455@gmail.com>
2023-09-09 14:30:27 -0400

    Laid groundwork for grappling, ungrappling, and reeling controls in comments

--- Assets/Scripts/Grappling.cs
+++ Assets/Scripts/Grappling.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,22 +13,31 @@ public class Grappling : MonoBehaviour
     public LayerMask hookable;
     public Transform hookPoint, camera, player;
     public RawImage crosshair;
-    private RopeController rope;
+    private SpringJoint joint;
     private Vector3 grapplePoint;
-    private bool grappling;
+    private bool isGrappling;
+    private bool grappleActive;
+    
+    [Header("Joint Mod")]
+    public float jointSpringForce;
+    public float jointDamperForce;
+    public float jointMassScale;
 
     [Header("Keybinds")] 
-    public KeyCode grappleKey;
+    public KeyCode grappleDirectionKey;
+
+    public KeyCode grappleKey = KeyCode.Mouse3;
+    public KeyCode reelKey = KeyCode.Mouse4;
     
     // Start is called before the first frame update
     void Start()
     {
-        grappling = false;
+        isGrappling = false;
+        grappleActive = false;
         
         hookPoint = transform.GetChild(1).GetComponent<Transform>();
         camera = GameObject.FindWithTag("Camera").transform;
         player = GameObject.FindWithTag("Player").transform;
-        rope = GetComponent<RopeController>();
     }
     
     void FixedUpdate()
@@ -43,10 +53,20 @@ public class Grappling : MonoBehaviour
             crosshair.color = Color.black;
         }
         
-        // Toggle grapple.
-        if (Input.GetKeyDown(grappleKey) && !grappling)
+        // Toggle grapple with grapple key held and grapple button (default thumb button front)
+        if ((Input.GetKey(grappleDirectionKey) && Input.GetKeyDown(grappleKey)) && !isGrappling)
         {
             StartCoroutine(Grapple(0.2f));
+        } 
+        // Toggle ungrapple with grapple key held and grapple button (default thumb button front)
+        else if (isGrappling)
+        {
+            Ungrapple();
+        }
+        // Reeling input: grapple key held down and set reel button (default thumb button back)
+        else if ()
+        {
+            
         }
     }
 
@@ -54,38 +74,56 @@ public class Grappling : MonoBehaviour
     // TODO: Currently can grapple with same key multiple times.
     IEnumerator Grapple(float waitTime)
     {
-        grappling = true;
+        isGrappling = true;
         
         RaycastHit hit;
         if (Physics.Raycast(camera.position, camera.forward, out hit, hookRange))
         {
-            rope.IncreaseRopeSegments();
+            grappleActive = true;
             
-            // grapplePoint = hit.point;
+            grapplePoint = hit.point;
             
             // Grapple joint
-            // joint = player.gameObject.AddComponent<SpringJoint>();
-            // joint.autoConfigureConnectedAnchor = false;
-            // joint.connectedAnchor = grapplePoint;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
 
             float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
             
             // Joint modification code:
-            // joint.spring = jointSpringForce;
-            // joint.damper = jointDamperForce;
-            // joint.massScale = jointMassScale;
+            joint.spring = jointSpringForce;
+            joint.damper = jointDamperForce;
+            joint.massScale = jointMassScale;
         }
         
         yield return new WaitForSeconds(waitTime);
         
-        grappling = false;
+        isGrappling = false;
     }
 
     // Also inspired by Dani's YouTube tutorial on grapple guns.
     void DrawRope()
     {
         // If not grappling, don't draw rope
-        // if (!joint) return;
+        if (!joint) return;
 
+        
+    }
+    
+    // Ungrapple, unhook joint
+    void Ungrapple()
+    {
+        Destroy(joint);
+    }
+
+    // Reeling is done by pressing the side you want to reel and holding the button you set (default thumb button back)
+    // Implementation wise, we need to do three things, push the player forwards, readjust the joint, and fix the line renderer.
+    // For each FixedUpdate() call, we do these:
+    // Push player forwards: push the player forwards with a force strength
+    // Readjust joint: reset the joint anchor to the hook point and our current position
+    // Fix line renderer: reset the line renderer to hook point and our current position
+    void Reel()
+    {
+        
     }
 }
